67031918a444941810a14d6d4002dd4c55a9fb34
Tmonster <tom@ebergen.com>
2024-03-07 13:11:30 +0100

    lower memory limits again (1GB/2GB)

--- setup-manual-limited.R
+++ setup-manual-limited.R
@@ -4,7 +4,7 @@ unlink("data.duckdb")
 con <- dbConnect(duckdb(dbdir = "data.duckdb"))
 dbExecute(con, paste0("PRAGMA temp_directory='", tempdir(), "'"))
 
-dbExecute(con, "PRAGMA memory_limit='3GB'")
+dbExecute(con, "PRAGMA memory_limit='1GB'")
 
 args <- commandArgs(TRUE)
 if (length(args) > 0) {
